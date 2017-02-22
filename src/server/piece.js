1aeca301b945918f5399d562f050852f2d9e6d43
abombard <arthbab@gmail.com>
2017-02-22 15:35:09 +0100

    piece refactoring

--- src/server/piece.js
+++ src/server/piece.js
@@ -1,8 +1,6 @@
-var Piece = function(x, y)  {
-  this.x = x;
-  this.y = y;
-  var figures = [];
-  figures["L"] = [
+const figures = [
+// L
+  [
     [
       [0,1,0],
       [0,1,0],
@@ -23,8 +21,9 @@ var Piece = function(x, y)  {
       [1,1,1],
       [0,0,1]
     ],
-  ];
-  figures["Z"] = [
+  ],
+// S
+  [
     [
       [1,1,0],
       [0,1,1],
@@ -35,8 +34,9 @@ var Piece = function(x, y)  {
       [1,1,0],
       [1,0,0]
     ],
-  ];
-  figures["T"] = [
+  ],
+// T
+  [
     [
       [0,1,0],
       [1,1,1],
@@ -57,8 +57,9 @@ var Piece = function(x, y)  {
       [1,1,0],
       [0,1,0]
     ],
-  ];
-  figures["I"] = [
+  ],
+// I
+  [
     [
       [0,0,0,0],
       [1,1,1,1],
@@ -71,46 +72,30 @@ var Piece = function(x, y)  {
       [0,0,1,0],
       [0,0,1,0]
     ],
-  ];
-  figures["O"] = [
+  ],
+// O
+  [
     [
       [1,1],
       [1,1]
-    ]
-  ];
-  var rand = Math.floor(Math.random() * 5) + 1;
-  switch (rand) {
-    case 1:
-      this.id = "L";
-      break;
-    case 2:
-      this.id = "Z"
-      break;
-    case 3:
-      this.id = "T"
-      break;
-    case 4:
-      this.id = "I"
-      break;
-    case 5:
-      this.id = "O"
-  }
-      this.rot = 0;
-      this.piece = figures[this.id][this.rot];
+    ],
+  ],
+]
 
-  this.rotate = function() {
-    if (figures[this.id][this.rot + 1] === undefined)
-    {
-      this.rot = 0;
-      this.piece = figures[this.id][this.rot]
-    }
-    else
-    {
-      this.rot += 1;
-      this.piece = figures[this.id][this.rot]
-    }
-    
-  }
-
+const getRandomPieceId = () => {
+  return Math.floor(Math.random() * 5);
 }
+
+var Piece = function(x, y)  {
+  this.x = x
+  this.y = y
+  this.id = getRandomPieceId()
+  this.rot = 0
+  this.piece = figures[this.id][this.rot]
+  this.rotate = () => {
+    this.rot = figures[this.id][this.rot + 1] === undefined ? 0 : this.rot + 1
+    this.piece = figures[this.id][this.rot]
+  }
+}
+
 module.exports = Piece;
