d81637291e4865168173822133345ba97ba7dbab
gonzo <blodnasir@gmail.com>
2017-02-23 15:07:31 +0100

    redux tests

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,3 +1,6 @@
+export const REQ_REFRESH_ROOMS = 'REQ_REFRESH_ROOMS'
+export const REFRESH_ROOMS = 'REFRESH_ROOMS'
+
 export const reqRefreshRooms = () => {
   return {
     type: 'REQ_REFRESH_ROOMS'
--- test/redux1.js
+++ test/redux1.js
@@ -1,23 +1,71 @@
 import {configureStore} from './helpers/server'
 import rootReducer from '../src/client/reducers'
-import {ALERT_POP, alert} from '../src/client/actions/alert'
+import {GET_MAP, getMap} from '../src/client/actions/getMap'
+import {REQ_REFRESH_ROOMS, REFRESH_ROOMS, reqRefreshRooms, refreshRooms} from '../src/client/actions/index'
 import chai from "chai"
 
-const MESSAGE = "message"
+const GRID = [ [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 7, 7, 7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ] ]
+
+const EMPTY_ROOMS = []
+
+const ROOM_LIST = [{name : "kitchen", playerCount: 2},{name : "livingroom", playerCount: 45}]
+
 
 chai.should()
 
-describe('Fake redux test', function(){
-  it('alert it', function(done){
+describe('map transmission', function(){
+  it('should get the map', function(done){
     const initialState = {}
     const store =  configureStore(rootReducer, null, initialState, {
-      ALERT_POP: ({dispatch, getState}) =>  {
+      GET_MAP: ({dispatch, getState}) =>  {
         const state = getState()
-        state.message.should.equal(MESSAGE)
+        state.grid.grid.should.equal(GRID)
         done()
       }
     })
-    store.dispatch(alert(MESSAGE))
+    store.dispatch(getMap(GRID))
+  });
+
+});
+
+
+
+describe('empty room list', function(){
+  it('should delete all rooms', function(done){
+    const initialState = {}
+    const store =  configureStore(rootReducer, null, initialState, {
+      REQ_REFRESH_ROOMS: ({dispatch, getState}) =>  {
+        const state = getState()
+        state.lobby.rooms.should.be.empty
+        done()
+      }
+    })
+    store.dispatch(reqRefreshRooms(REQ_REFRESH_ROOMS))
+  });
+
+});
+
+
+describe('retrieve room list', function(){
+  it('should get the room list from server', function(done){
+    const initialState = {}
+    const store =  configureStore(rootReducer, null, initialState, {
+      REFRESH_ROOMS: ({dispatch, getState}) =>  {
+        const state = getState()
+        state.lobby.rooms.should.equal(ROOM_LIST)
+        done()
+      }
+    })
+    store.dispatch(refreshRooms(ROOM_LIST))
   });
 
 });
