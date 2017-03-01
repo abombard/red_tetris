d9237941053133d77970c42bedf138b34186ae78
adrien <aaure@student.42.fr>
2017-03-01 16:37:40 +0100

    [Upd] package.json now multi-os ; [Add] 3D blocks in CSS

--- src/client/components/GridCell.js
+++ src/client/components/GridCell.js
@@ -2,19 +2,22 @@ import React, { PropTypes } from 'react'
 
 const colorMap = [
   'blank',
-  'blue',
-  'green',
-  'red',
-  'yellow',
-  'magenta',
-  'brown',
-  'grey',
-  'black'
+  'orange-piece', // L inversé
+  'yellow-piece', // L
+  'red-piece', // Z
+  'green-piece', // S
+  'pink-piece',  // T
+  'light-blue-piece', // I
+  'blue-piece', // carré
+  'grey'
 ]
 colorMap[42] = 'black'
 
 const GridCell = ({ color }) => (
-  <div className={`GridCell ${colorMap[color]}`} />
+  <div className={`GridCell ${colorMap[color]}`}>
+    <div className={`innerGridCell ${colorMap[color]}`}>
+    </div>
+  </div> 
 )
 
 GridCell.propTypes = {
